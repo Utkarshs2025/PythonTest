7c552d060db964166834a3cdcfff89ee84a0a0d4
Utkarshs2025 <Utkarshs2025@gmail.com>
2023-01-21 20:15:16 +0530

    initial commit

---
+++ One.py
@@ -0,0 +1,13 @@
+
+
+'''
+@author Swati Shikha
+'''
+
+def calculate_area(radius):
+    area=3.14*radius*radius
+    return  area
+
+value=calculate_area(5)
+
+print('The area of Circle is :'+str(value)+'cm square')
